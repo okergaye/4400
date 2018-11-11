879dcb7276dc92d32dcb57183beb7e03ff16db6e
osamak <osamak@lab1-37.eng.utah.edu>
2018-11-11 16:23:50 -0700

    malloc commented

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -1,13 +1,20 @@
 #include "mm.h"
 
 #define ALIGNMENT 16
+//Alignes to the next 16 byte alinment
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
-
+// returns the size of the block_header ()
 #define OVERHEAD sizeof(block_header)
+//Macro for getting the header from a payload pointer:
+/// bp i think is block payload, and is a pointer
 #define HDRP(bp) ((char *)(bp) - sizeof(block_header))
 
+//these are used on block header pointers to see the size of 
+//the block and weather is it allocated
 #define GET_SIZE(p)  ((block_header *)(p))->size
 #define GET_ALLOC(p) ((block_header *)(p))->allocated
+
+//Macro for getting the next block’s payload:
 
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
 
