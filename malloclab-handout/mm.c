a54ee6b99b7a1d35c25eb8a6e3c54a691eee95e6
osamak <osamak@lab1-37.eng.utah.edu>
2018-11-13 14:18:05 -0700

    got to 60!

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -28,17 +28,45 @@ void *first_bp;
 void mm_init(void *heap, size_t heap_size)
 {
   void *bp;
+  //make terminator
+  bp = heap+(heap_size - OVERHEAD);
+  GET_SIZE(HDRP(bp)) =  OVERHEAD;
+  GET_ALLOC(HDRP(bp)) = 1;
+
+  //make prolog boi
+  bp = heap + OVERHEAD; // header
+  GET_SIZE(HDRP(bp)) = OVERHEAD*2;
+  GET_ALLOC(HDRP(bp)) = 1;
+  bp = heap + OVERHEAD;//footer
+  GET_SIZE(HDRP(bp)) = OVERHEAD*2;
+
+  //next free 
+  bp = heap + OVERHEAD;
+
+
+
+
+  // bp = heap + OVERHEAD;
   
-  bp = heap + sizeof(block_header);
-  
-  GET_SIZE(HDRP(bp)) = heap_size;
+  GET_SIZE(HDRP(bp)) = heap_size - (OVERHEAD*3);
   GET_ALLOC(HDRP(bp)) = 0;
 
   first_bp = bp;
 }
 
-static void set_allocated(void *bp)
+// static void set_allocated(void *bp)
+// {
+//   GET_ALLOC(HDRP(bp)) = 1;
+// } //make change later
+void set_allocated(void *bp, size_t size)
 {
+  size_t extra_size = GET_SIZE(HDRP(bp)) - size;
+  if (extra_size > ALIGN(1 + OVERHEAD))
+  {
+    GET_SIZE(HDRP(bp)) = size;
+    GET_SIZE(HDRP(NEXT_BLKP(bp))) = extra_size;
+    GET_ALLOC(HDRP(NEXT_BLKP(bp))) = 0;
+  }
   GET_ALLOC(HDRP(bp)) = 1;
 }
 
@@ -46,16 +74,34 @@ void *mm_malloc(size_t size)
 {
   void *bp = first_bp;
 
-  if (GET_ALLOC(HDRP(bp))) {
-    /* Our only block is already allocated tes2*/
-    return NULL;
-  } else {
-    set_allocated(bp);
-    return bp;
+  // if (GET_ALLOC(HDRP(bp))) {
+  //   /* Our only block is already allocated tes2*/
+  //   return NULL;
+  // } else {
+  //   set_allocated(bp);
+  //   return bp;
+  // }
+void *best_bp = NULL;
+ int new_size = ALIGN(size + OVERHEAD);
+
+  while (GET_SIZE(HDRP(bp)) != 0)
+  {
+    if (!GET_ALLOC(HDRP(bp)) && (GET_SIZE(HDRP(bp)) >= new_size))
+    {
+      if (!best_bp || (GET_SIZE(HDRP(bp)) < GET_SIZE(HDRP(best_bp))))
+        best_bp = bp;
+    }
+    bp = NEXT_BLKP(bp);
+  }
+  if (best_bp)
+  {
+    set_allocated(best_bp, new_size);
+    return best_bp;
   }
 }
 
 void mm_free(void *bp)
 {
   GET_ALLOC(HDRP(bp)) = 0;
+  // coalesce(bp);
 }
